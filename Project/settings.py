aa7f40173294d003d803cb7fa2b8a14e78ae206f
Andrew Ryan <aryan17@mail.csuchico.edu>
2017-05-11 11:52:36 -0700

    working on redis

--- Project/settings.py
+++ Project/settings.py
@@ -46,7 +46,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'asgi_redis.RedisChannelLayer',
         'CONFIG': {
-            'hosts': ["redis://redis-channel-1:6379/0"],
+            'hosts': [('redis', 6379)],
         },
         'ROUTING': 'Project.routing.channel_routing',
     }
